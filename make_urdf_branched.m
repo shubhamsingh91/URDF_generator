939d8b81fae9eff23ba621dfb4983c7b3d6e2f22
shubhamsingh91 <shubham.singh091@gmail.com>
2021-12-03 16:50:15 -0600

    update for 1 link

--- make_urdf_branched.m
+++ make_urdf_branched.m
@@ -1,11 +1,20 @@
-function out = make_urdf(n)
+function out = make_urdf_branched(n,bf)
 
-parent_vec = [0:1:n-1];
-child_vec = [1:1:n];
 
+for ii=1:n
+
+    parent_vec(ii) = floor((ii-2+ceil(bf))/bf);
+
+end
+
+for ii=1:n
+    
+    child_vec(ii) = ii;
+
+end
 %% 
 
-robot_name = strcat(num2str(n),'link');
+robot_name = strcat(num2str(n),'link_bf_',num2str(bf));
 filename = strcat(robot_name,'.urdf');
 
 fileID = fopen(filename,'w');
@@ -22,25 +31,19 @@ fprintf(fileID,str1);
 %% dictionary for child/parent
 
 for ii=1:n
-   
-    if parent_vec(ii)==0
+ 
+   if parent_vec(ii)==0
      parent_vec_name{ii} = '"base_link"';
-    else
+   else
      parent_vec_name{ii}=strcat('"link',num2str(parent_vec(ii)),'"');
-    end
-    
-    child_vec_name{ii} = strcat('"link',num2str(child_vec(ii)),'"');
-    
+   end
+     child_vec_name{ii} = strcat('"link',num2str(child_vec(ii)),'"');
+
 end
 
-if n==1
-      parent = containers.Map(parent_vec,parent_vec_name{1});
-  child = containers.Map(child_vec,child_vec_name{1});
-else
   parent = containers.Map(parent_vec,parent_vec_name);
   child = containers.Map(child_vec,child_vec_name);
-end
-
+  
 %% link info- base link
 
 link_name = '"base_link"';
@@ -64,6 +67,7 @@ fprintf(fileID,str_link);
 
 % assuming all links of length = 1
 
+l=1;
 
 for ii=1:n
     
@@ -71,7 +75,7 @@ for ii=1:n
     % link info
     
     input.link_name = strcat('"link',num2str(ii),'"');
-    input.link_xyz_vec = [ii-1,0,0];
+    input.link_xyz_vec = [parent_vec(ii)*l,0,0];
     input.link_rpy_vec = [0,0,0];
     input.link_mass_num=1.5;
     input.link_inertia_mat = [0,0,0,0,0,1];
@@ -90,10 +94,10 @@ for ii=1:n
 
     input_jt.joint_name = strcat('"joint',num2str(ii),'"');
     input_jt.joint_type = strcat('"revolute"');
-    input_jt.joint_xyz_vec = [ii-1,0,0];
+    input_jt.joint_xyz_vec = [parent_vec(ii)*l,0,0];
     input_jt.joint_rpy_vec = [0,0,0];
-    input_jt.joint_parent = parent(ii-1);
-    input_jt.joint_child = child(ii);
+    input_jt.joint_parent = parent(parent_vec(ii));
+    input_jt.joint_child = child(child_vec(ii));
     input_jt.joint_axis = [0,0,1]; % reovlute z axis joint here
     input_jt.limits = [0,0,0,0]; % lower, upper, effort, vecloity
     input_jt.damping = 0.05; % damping
--- urdf_branched.m
+++ urdf_branched.m
@@ -3,3 +3,15 @@ clc; clear all; close all;
 %% For branched URDFs
 
 
+%% Adding spatial_v2 to the MATLAB path
+
+% Determine where your m-file's folder is.
+folder = fileparts(which(mfilename)); 
+% % Add that folder plus all subfolders to the path.
+addpath(genpath(folder));
+
+n = 5;
+bf = 2;
+
+make_urdf_branched(n,bf)
+
